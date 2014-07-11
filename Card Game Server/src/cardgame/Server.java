64772762848e73ce37d5122348b8ad1f3b7954d1
Junior <junior-verswijver@hotmail.com>
2014-07-11 21:47:44 +0200

    Input and output for server, chat system

--- Card Game Server/src/cardgame/Server.java
+++ Card Game Server/src/cardgame/Server.java
@@ -9,16 +9,62 @@ public class Server {
 	static ServerSocket serverSocket;
 	static Socket socket;
 	static DataOutputStream out;
+	static DataInputStream in;
+	static Users[] user = new Users[10];
 	
 	public static void main(String[] args) throws Exception {
 		System.out.println("Starting server...");
 		serverSocket = new ServerSocket(7777);
 		System.out.println("Server started!");
-		socket = serverSocket.accept();
-		System.out.println("Connection from: " + socket.getInetAddress());
-		out = new DataOutputStream(socket.getOutputStream());
-		out.writeUTF("Test test");
-		System.out.println("Data has been sent");
+		while(true){
+			socket = serverSocket.accept();
+			for(int i=0; i<10; i++){
+				System.out.println("Connection from: " + socket.getInetAddress());
+				out = new DataOutputStream(socket.getOutputStream());
+				in = new DataInputStream(socket.getInputStream());
+				if(user[i]==null){
+					user[i] = new Users(out, in, user);
+					Thread thread = new Thread(user[i]);
+					thread.start();
+					break;
+				}
+			}
+		}
+	}
+}
+
+class Users implements Runnable{
+	
+	DataOutputStream out;
+	DataInputStream in;
+	Users[] user = new Users[10];
+	String name;
+	
+	public Users(DataOutputStream out, DataInputStream in, Users[] user){
+		this.out = out;
+		this.in = in;
+		this.user = user;
+	}
+
+	public void run() {
+		try {
+			name = in.readUTF();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		while(true){
+			try {
+				String message = in.readUTF();
+				for(int i=0; i<10;i++){
+					if(user[i] != null){
+						user[i].out.writeUTF(name + ":" + message);
+					}
+				}
+			} catch (IOException e) {
+				this.out = null;
+				this.in = null;
+			}
+		}
 	}
 	
 }
