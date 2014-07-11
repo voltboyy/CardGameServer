7f9bce84800793d6074fafb773a6a789f02d63ae
Junior <junior-verswijver@hotmail.com>
2014-07-11 20:53:27 +0200

    Server side data sending/retrieving basics added

--- Card Game Server/src/cardgame/Server.java
+++ Card Game Server/src/cardgame/Server.java
@@ -1,5 +1,24 @@
 package cardgame;
 
-public class Server {
+import java.net.ServerSocket;
+import java.io.*;
+import java.net.*;
 
+public class Server {
+	
+	static ServerSocket serverSocket;
+	static Socket socket;
+	static DataOutputStream out;
+	
+	public static void main(String[] args) throws Exception {
+		System.out.println("Starting server...");
+		serverSocket = new ServerSocket(7777);
+		System.out.println("Server started!");
+		socket = serverSocket.accept();
+		System.out.println("Connection from: " + socket.getInetAddress());
+		out = new DataOutputStream(socket.getOutputStream());
+		out.writeUTF("Test test");
+		System.out.println("Data has been sent");
+	}
+	
 }
